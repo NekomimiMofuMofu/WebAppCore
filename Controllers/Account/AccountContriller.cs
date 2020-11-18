8ef11d6a4c931a81a14ceedf352c83387ab07560
raiti-chan <rusuke@t-net.ne.jp>
2020-11-18 14:00:40 +0900

    #0002 リファクタリング

--- Controllers/Account/AccountContriller.cs
+++ Controllers/Account/AccountContriller.cs
@@ -3,6 +3,8 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
+#nullable enable
+
 namespace WebAppCore.Controllers.Account {
 	[ApiController]
 	[Route("api/[controller]")]
@@ -12,35 +14,30 @@ namespace WebAppCore.Controllers.Account {
 			AccountItem item = new AccountItem();
 			try {
 				string decode = Encoding.UTF8.GetString(Convert.FromBase64String(request));
-				string id = decode[..decode.IndexOf('%')];
-				if (id.Equals(string.Empty)) {
-					item.Id = "N/a";
-					item.AccessToken = "N/a";
-					item.StatusCode = HttpStatusCode.BadRequest;
-					Response.StatusCode = (int)HttpStatusCode.BadRequest;
-					return item;
-				}
-				item.Id = id;
-				
-				string password = decode[(decode.IndexOf('%') + 1)..];
-				if (password.Equals(string.Empty)) {
-					item.AccessToken = "N/a";
-					item.StatusCode = HttpStatusCode.BadRequest;
-					Response.StatusCode = (int) HttpStatusCode.BadRequest;
-					return item;
-				}
-
-				item.AccessToken = password;
-				item.StatusCode = HttpStatusCode.OK;
-
+				item.Id = decode[..decode.IndexOf('%')];
+				item.AccessToken = decode[(decode.IndexOf('%') + 1)..];
+				item.StatusCode = AccountItemErrorCheck(item);
+				Response.StatusCode = (int)item.StatusCode;
 			} catch {
-				item.Id = "N/a";
-				item.AccessToken = "N/a";
+				item.Id = "Error";
+				item.AccessToken = "Error";
 				item.StatusCode = HttpStatusCode.BadRequest;
 				Response.StatusCode = (int)HttpStatusCode.BadRequest;
 			}
 
 			return item;
+		}
+
+		/// <summary>
+		/// アカウント情報アイテムに不整合がないかチェック
+		/// </summary>
+		/// <param name="item">アカウント情報</param>
+		/// <returns>不整合があった場合<see cref="HttpStatusCode.BadRequest"/></returns>
+		private static HttpStatusCode AccountItemErrorCheck(AccountItem item) {
+			if (item.Id.Equals(string.Empty) || item.AccessToken.Equals(string.Empty)) {
+				return HttpStatusCode.BadRequest;
+			}
+			return HttpStatusCode.OK;
 		}
 	}
 }
