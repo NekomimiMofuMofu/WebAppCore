0c794d95ad3be45f40192821d29ab89217cd0a94
bakhyun-lee <k019c1291@it-neec.jp>
2020-12-02 14:32:21 +0900

    Revert "ボタンの追加"
    This reverts commit cfaed5f1364d55d1b42328cf34e1c5e6f276932e.

--- vue_client/src/views/LoginPage.vue
+++ vue_client/src/views/LoginPage.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
+    <h1>ログイン</h1>
     <dl class="login_input">
-      <dt><label for="i_user_id">UserName</label></dt>
-      <dt><input id="i_user_id" type="text" placeholder="" name="i_user_id" v-model="user_id"></dt>
-      <dt><label for="i_password">PassWord</label></dt>
-      <dt><input id="i_password" type="password" placeholder="" name="i_password" v-model="password"></dt>
-      <dt>
-        <button id="btn_signIn" type="button" @click="onClick1" name="btn_signIn" >Sign In</button>
-        <button id="btn_signUp" type="button" @click="onClick2" name="btn_signUp" >Sign Up</button>
-      </dt>
+      <dt><label for="i_user_id">ユーザーID</label></dt>
+      <dt><input id="i_user_id" type="text" placeholder="ユーザーID" name="i_user_id" v-model="user_id"></dt>
+      <dt><label for="i_password">パスワード</label></dt>
+      <dt><input id="i_password" type="password" placeholder="パスワード" name="i_password" v-model="password"></dt>
+      <dt><button id="btn_login" type="button" @click="onClick" name="btn_login" >ログイン</button></dt>
     </dl>
   </div>
 </template>
@@ -27,14 +24,13 @@ export default class LoginPage extends Vue {
 
   private error_message = "IDかパスワードが間違ってるよ";
   
-  private onClick1(): void {
+  private onClick(): void {
     if(this.user_id == this.id_validate && this.password == this.pass_validate){
       this.$router.push("/about");
     }else {
       alert(this.error_message);
     }
   }
-  
 }
 
 </script>
@@ -42,7 +38,7 @@ export default class LoginPage extends Vue {
 <style scoped lang="scss">
 .login {
   margin: 100px;
-  padding: 30px 60px;
+  padding: 20px;
   
   .login_input {
     
@@ -52,7 +48,6 @@ export default class LoginPage extends Vue {
 
 h1 {
   color: #76f8e2;
-  font-size: xxx-large;
 }
 
 label {
@@ -61,10 +56,6 @@ label {
 }
 
 input {
-  
-}
-
-button {
   
 }
 
