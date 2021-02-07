7eab3dcd92944927c7bbda5bf5abd08253705d46
sin_ni <0bl6zqh7li@gmail.com>
2021-02-07 22:17:19 +0900

    ユーザーテーブルにnameとmailとpass、収納テーブルとアイテムテーブルの初期化SQLを追加

--- SQL/InitMock.sql
+++ SQL/InitMock.sql
@@ -4,7 +4,22 @@ DELETE FROM * ;
 DROP TABLE * ;
 
 /* ユーザーテーブルの定義 */
-
 CREATE TABLE user {
-	u_id	INTEGER
+	u_id   INTEGER,
+	u_name CHAR(30),
+	mail   CHAR(20)
+	pass   CHAR(8)
+};
+
+/* 収納テーブルの定義 */
+CREATE TABLE drawr {
+	d_id   INTEGER,
+	d_name CHAR(30),
+	i_id   INTEGER
+};
+
+/* アイテムテーブルの定義 */
+CREATE TABLE item {
+	i_id   INTEGER,
+	i_name CHAR(30)
 };
